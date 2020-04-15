87e381541331eec340164ab6e16ad8faa425ec17
Samveg Thaker <thakersamveg608@gmail.com>
2020-04-15 19:06:58 +0530

    Axios integrated

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,51 @@
-import React from 'react'
+import React, {Component} from 'react'
+import axios from 'axios'
+import {Link} from "react-router-dom";
+import Pokeball from '../pokeball.png'
 
-const Home = () => {
-    return(
-        <div className="container">
-            <h4 className="center">Home</h4>
-            <p>lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum </p>
-        </div>
-    )
+class Home extends  Component {
+ 
+    state = {
+        posts: [ ]
+    }
+
+    componentDidMount() {
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then(res => {
+                console.log(res);
+                this.setState({
+                    posts: res.data.slice(0, 10)
+                })
+            })
+    }
+
+    render() {
+        const {posts} = this.state
+        const postList = posts.length ? (
+            posts.map(post => {
+                return(
+                    <div className="post card" key={post.id}>
+                        <img src={Pokeball} alt="A Pokeball" />
+                        <div className="card-content">
+                            <Link to={"/posts/"+post.id}>
+                                <span className="card-title" >{post.title}</span>
+                            </Link>
+                            <p>{post.body}</p>
+                        </div>
+
+                    </div>
+                )
+            })
+        ):(
+            <div className="center">No posts yet</div>
+        )
+        return(
+            <div className="container home ">
+                <h4 className="center">Home</h4>
+                {postList }
+            </div>
+        )
+    }
 }
 
 export default Home
