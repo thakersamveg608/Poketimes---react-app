ab3b4b7bafb596cada46efb9aee0af1f6941b142
Samveg Thaker <thakersamveg608@gmail.com>
2020-04-16 00:27:02 +0530

    Actions integrated

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,6 +1,7 @@
 import React, {Component} from "react"
 // import axios from 'axios'
 import { connect } from 'react-redux'
+import {deletePost} from "../actions/postActions";
 
 class Post extends Component{
 
@@ -20,6 +21,11 @@ class Post extends Component{
     //         })
     // }
 
+    handleClick = () => {
+        this.props.deletePost(this.props.post.id);
+        this.props.history.push('/');
+    }
+
     render() {
         // const post = this.state.post ? (
         console.log(this.props)
@@ -29,6 +35,11 @@ class Post extends Component{
                 {/*<p>{this.state.post.body}</p>*/}
                 <h4 className='center'>{this.props.post.title}</h4>
                 <p>{this.props.post.body}</p>
+                <div className="center">
+                    <button className="btn grey" onClick={this.handleClick}>
+                        Delete Post
+                    </button>
+                </div>
             </div>
         ) : (
             <div className='center'>Loading Post...</div>
@@ -43,10 +54,16 @@ class Post extends Component{
 
 const mapStateToProps = (state, ownProps) => {
     let id = ownProps.match.params.post_id;
-    console.log(id)
     return {
-        post: state.posts.find(post => post.id===id)
+        post: state.posts.find(post => post.id === Number (id))
     }
 }
 
-export default connect(mapStateToProps)(Post)
+const mapDispatchToProps = (dispatch) => {
+    return {
+        deletePost: (id) => { dispatch(deletePost(id))}
+    }
+}
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(Post)
